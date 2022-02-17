9945f9afaebbe21893b284bc88ad48d1b6af3af5
Nick Raffaele <Raffaele.nickp@gmail.com>
2022-02-17 17:10:59 -0500

    Test route

--- restapi/server.js
+++ restapi/server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const corsConfig = {
-  origin: "http://localhost:3000",
+origin: "http://localhost:3000",
 };
 
 app.use(cors(corsConfig));
@@ -18,6 +18,9 @@ app.use("/payee", routes.payee);
 app.use("/transactions", routes.transactions);
 app.use("/users", routes.users);
 
+app.use("/test",(req,res) => {
+res.json({message:"hello"})
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
