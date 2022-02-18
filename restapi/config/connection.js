0303b6044451768297c4bbb35af3d2b12d4bba11
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-02-18 14:56:05 -0600

    breaking the pipeline

--- restapi/config/connection.js
+++ restapi/config/connection.js
@@ -4,11 +4,11 @@ const connection = {};
 
 connection.string = process.env.DATABASE_CONNECTION_STRING;
 connection.object = {
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASS,
-    port: process.env.DATABASE_PORT,
+host: process.env.DATABASE_HOST,
+database: process.env.DATABASE_NAME,
+user: process.env.DATABASE_USER,
+password: process.env.DATABASE_PASS,
+port: process.env.DATABASE_PORT,
 };
 
 export default connection;
