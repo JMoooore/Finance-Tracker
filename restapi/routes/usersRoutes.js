f7090b8b8b6ffad59de8dabb74c6393dc8b99693
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-02-18 17:21:42 -0600

    Single user request bug

--- restapi/routes/usersRoutes.js
+++ restapi/routes/usersRoutes.js
@@ -1,21 +1,11 @@
 import { Router, json } from 'express';
-import db from '../model/connection.js';
 import userController from '../controllers/userController.js';
-
 
 const users = new Router();
 users.use(json());
 
 users.get('/', userController.getAll);
 
-users.get('/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const result = await db('users').where('id', id);
-        res.json(result);
-    } catch (err) {
-        errorHandler(err, res);
-    }
-});
+users.get('/:id', userController.getOne);
 
 export default users;
