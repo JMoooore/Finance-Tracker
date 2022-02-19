fe3c7cac5d9d5d52bee123a997e867caad9a08d3
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-02-18 18:10:17 -0600

    Ran prettier

--- restapi/controllers/userController.js
+++ restapi/controllers/userController.js
@@ -1,20 +1,20 @@
-import userModel from "../model/usersModel.js";
+import userModel from '../model/usersModel.js';
 
-const userController = {}
+const userController = {};
 
-userController.getAll = async (req,res) => {
-    const data = await userModel.getAll()
-    res.json(data)
-}
+userController.getAll = async (req, res) => {
+    const data = await userModel.getAll();
+    res.json(data);
+};
 
-userController.getOne = async (req,res) => {
-    const data = await userModel.getOne(req.params.id)
-    res.json(data)
-}
+userController.getOne = async (req, res) => {
+    const data = await userModel.getOne(req.params.id);
+    res.json(data);
+};
 
-userController.post = async (req,res) => {
-    const data = await userModel.post(req.body)
-    res.json(data)
-}
+userController.post = async (req, res) => {
+    const data = await userModel.post(req.body);
+    res.json(data);
+};
 
-export default userController
+export default userController;
