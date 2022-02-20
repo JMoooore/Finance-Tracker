2678409c1f3bb6159a5078ec14e0650dfcb10a93
Nick Raffaele <Raffaele.nickp@gmail.com>
2022-02-19 21:32:52 -0500

    Format

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import Table from "./components/Table.jsx";
-import NavBar from "./components/navbarComponent/Navbar.jsx";
+import Table from './components/Table.jsx';
+import NavBar from './components/navbarComponent/Navbar.jsx';
 import './App.css';
-import './colors.css'
+import './colors.css';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <NavBar/>
-        {/* <Table/> */}
+    return (
+        <Router>
+            <div className="App">
+                <NavBar />
+                {/* <Table/> */}
 
-        <Routes> 
-          <Route path="/navbar" element={<NavBar />} />
-          <Route path="/transactions" element={<Table />} />
-        </Routes>
-
-      </div>
-    </Router>
-  );
+                <Routes>
+                    <Route path="/navbar" element={<NavBar />} />
+                    <Route path="/transactions" element={<Table />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
