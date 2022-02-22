c75590349a9795cf8b039dbe33d35cd94f4b2509
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-02-22 20:53:24 +0000

    User API Routes/Controller/Model

--- restapi/model/usersModel.js
+++ restapi/model/usersModel.js
@@ -12,13 +12,43 @@ userModel.getOne = async (id) => {
     return rows;
 };
 
-userModel.post = async (body) => {
+userModel.createOne = async (body) => {
     const { first_name, last_name, email, password } = body;
-    await db.query(
-        'INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)',
+    const { rows } = await db.query(
+        'INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *',
         [first_name, last_name, email, password]
     );
-    return { message: 'New User Added' };
+    return rows[0];
+};
+
+userModel.updateOne = async (id, body) => {
+    const { first_name, last_name, email, password } = body;
+    const user = await userModel.getOne(id);
+    const updateObj = {
+        first_name: first_name ?? user[0].first_name,
+        last_name: last_name ?? user[0].last_name,
+        email: email ?? user[0].email,
+        password: password ?? user[0].password,
+    };
+    const { rows } = await db.query(
+        'UPDATE users SET first_name=$1, last_name=$2, email=$3, password=$4 WHERE id=$5 RETURNING *',
+        [
+            updateObj.first_name,
+            updateObj.last_name,
+            updateObj.email,
+            updateObj.password,
+            id,
+        ]
+    );
+    return rows[0];
+};
+
+userModel.removeOne = async (id) => {
+    const { rows } = await db.query(
+        'DELETE FROM users WHERE id = $1 RETURNING *',
+        [id]
+    );
+    return rows[0];
 };
 
 export default userModel;
