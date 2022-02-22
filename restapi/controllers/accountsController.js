90ec7d420a864274874576ea5f803062a8c3711b
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-02-22 16:06:22 -0600

    Account POST Model, Controller, route

--- restapi/controllers/accountsController.js
+++ restapi/controllers/accountsController.js
@@ -1,4 +1,4 @@
-import accountModel from '../model/accountsModel';
+import accountModel from "../model/accountsModel.js";
 
 const accountController = {};
 
@@ -6,5 +6,10 @@ accountController.getAllByUser = async (req, res) => {
     const data = await accountModel.getAllByUser(req.params.id);
     res.json(data);
 };
+
+accountController.createNewAccount = async (req, res) => {
+    const data = await accountModel.createNewAccount(req.params.id, req.body);
+    res.json(data);
+}
 
 export default accountController;
