98f8555a6c72620497c120885d1fbf534eb1458b
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-02-22 16:31:13 -0600

    Account PATCH Model, Controller, Route

--- restapi/model/accountsModel.js
+++ restapi/model/accountsModel.js
@@ -2,6 +2,11 @@ import db from './connection.js';
 
 const accountModel = {};
 
+accountModel.getSingleAccount = async (id) => {
+    const {rows} = await db.query('SELECT * FROM accounts WHERE id=$1', [id]);
+    return rows;
+}
+
 accountModel.getAllByUser = async (id) => {
     const { rows } = await db.query('SELECT * FROM accounts WHERE user_id=$1', [
         id,
@@ -9,9 +14,31 @@ accountModel.getAllByUser = async (id) => {
     return rows;
 };
 
+accountModel.getAllAccounts = async () => {
+    const {rows} = await db.query('SELECT * FROM accounts');
+    return rows;
+}
+
 accountModel.createNewAccount = async (id, body) => {
     const {balance, name} = body
     const {rows} = await db.query('INSERT INTO accounts (user_id, name, balance) VALUES ($1, $2, $3) RETURNING *', [id, name, balance])
+    return rows[0];
+}
+
+accountModel.updateAccount = async (account_id, body) => {
+    const {balance, name} = body
+    const account = await accountModel.getSingleAccount(account_id);
+    const updateObj = {
+        name: name ?? account[0].name,
+        balance: balance ?? account[0].balance
+    };
+    const {rows} = await db.query(
+        'UPDATE accounts SET name=$1, balance=$2 WHERE id=$3 RETURNING *', 
+        [
+            updateObj.name,
+            updateObj.balance,
+            account_id
+        ]);
     return rows[0];
 }
 
--- restapi/routes/accountsRoutes.js
+++ restapi/routes/accountsRoutes.js
@@ -4,7 +4,12 @@ import accountController from '../controllers/accountsController.js';
 const accounts = new Router();
 accounts.use(json());
 
-accounts.get('/users/:id', accountController.getAllByUser);
-accounts.post('/users/:id', accountController.createNewAccount);
+accounts.get('/', accountController.getAllAccounts);
+
+accounts.get('/users/:user_id', accountController.getAllByUser);
+
+accounts.post('/users/:user_id', accountController.createNewAccount);
+
+accounts.patch('/users/:user_id/:account_id', accountController.updateAccount);
 
 export default accounts;
