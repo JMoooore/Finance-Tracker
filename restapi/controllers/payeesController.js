9a270c4f066ae97208f4d4601ada0732cdd3104c
John Moore <johnhtmoore@gmail.com>
2022-02-23 00:03:15 +0000

    Resolve "Standardize Payee Routes"

--- restapi/controllers/payeesController.js
+++ restapi/controllers/payeesController.js
@@ -3,17 +3,12 @@ import payeesModel from '../model/payeesModel.js';
 const payeesController = {};
 
 payeesController.getAll = async (req, res) => {
-    const data = await payeesModel.getAll(req.params.id);
-    res.json(data);
-};
-
-payeesController.getOne = async (req, res) => {
-    const data = await payeesModel.getOne(req.params.payee_id);
+    const data = await payeesModel.getAll(req.params.user_id);
     res.json(data);
 };
 
 payeesController.addOne = async (req, res) => {
-    const data = await payeesModel.addOne(req.params.id, req.body);
+    const data = await payeesModel.addOne(req.params.user_id, req.body);
     res.json(data);
 };
 
