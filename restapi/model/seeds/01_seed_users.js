e1192246401a5e90fb6b03c7d7aa2cfff8c89043
John Moore <johnhtmoore@gmail.com>
2022-02-23 16:58:37 -0700

    added hashing for seed file

--- restapi/model/seeds/01_seed_users.js
+++ restapi/model/seeds/01_seed_users.js
@@ -1,7 +1,12 @@
+import bcryptjs from "bcryptjs"
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
+
+const password = bcryptjs.hashSync('1234', 10)
+
 export function seed(knex) {
     return knex('users')
         .del()
@@ -11,37 +16,37 @@ export function seed(knex) {
                     first_name: 'Isaias',
                     last_name: 'Nuno',
                     email: 'in@email.com',
-                    password: '1234',
+                    password: password,
                 },
                 {
                     first_name: 'Kevin',
                     last_name: 'Heleodoro',
                     email: 'kh@email.com',
-                    password: '1234',
+                    password: password,
                 },
                 {
                     first_name: 'Nick',
                     last_name: 'Raffaele',
                     email: 'nr@email.com',
-                    password: '1234',
+                    password: password,
                 },
                 {
                     first_name: 'Fernando',
                     last_name: 'Curiel',
                     email: 'fc@email.com',
-                    password: '1234',
+                    password: password,
                 },
                 {
                     first_name: 'Paul',
                     last_name: 'Devlin',
                     email: 'pd@email.com',
-                    password: '1234',
+                    password: password,
                 },
                 {
                     first_name: 'John',
                     last_name: 'Moore',
                     email: 'jm@email.com',
-                    password: '1234',
+                    password: password,
                 },
             ]);
         });
