e78746695c4d263c01d1b0bfd164988dc18d3a94
Nick Raffaele <Raffaele.nickp@gmail.com>
2022-02-23 17:05:21 -0500

    refactored connection.js

--- restapi/model/connection.js
+++ restapi/model/connection.js
@@ -1,12 +1,8 @@
 import pg from 'pg';
 
 const connectionString = process.env.DATABASE_URL;
+const pfConfig = { connectionString };
 
-const pool = new pg.Pool({
-    connectionString,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-});
+const pool = new pg.Pool(pfConfig);
 
 export default pool;
