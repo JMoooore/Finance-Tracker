74f1b6b71883e3db572ebd5280e406424c2c97d3
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-02-23 17:44:25 -0600

    Table schema created

---
+++ restapi/model/rawSQL/schema/schema.sql
@@ -0,0 +1,40 @@
+\c sdc
+
+CREATE TABLE users(
+	id serial PRIMARY KEY,
+	first_name VARCHAR(255),
+	last_name VARCHAR(255),
+	password VARCHAR(255),
+	email VARCHAR(255)
+);
+
+CREATE TABLE accounts(
+	id serial PRIMARY KEY,
+	user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	name VARCHAR(100),
+	balance MONEY
+);
+
+CREATE TABLE payees(
+	id serial PRIMARY KEY,
+	user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	name VARCHAR(100)
+);
+
+CREATE TABLE categories(
+	id serial PRIMARY KEY,
+	user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	name VARCHAR(100)
+);
+
+CREATE TABLE transactions(
+	id serial PRIMARY KEY,
+	user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	payee_id INT REFERENCES payees(id) ON DELETE CASCADE,
+	account_id INT NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
+	category_id INT REFERENCES categories(id) ON DELETE CASCADE,
+	date DATE,
+	inflow MONEY,
+	outflow MONEY,
+	note TEXT
+);
