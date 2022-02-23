592e759e5e4230ce9d9952641f278f984165e29a
fernandocm9 <88690826+fernandocm9@users.noreply.github.com>
2022-02-22 20:12:24 -0600

    created the basis for the homepage

---
+++ frontend/src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Homepage.css'
+
+export default function Homepage() {
+  return (
+    <>
+        <div className='topImage' ></div>
+        {/* Login component */}
+        <div  className='bottomImage' ></div>
+    </>
+  )
+}
