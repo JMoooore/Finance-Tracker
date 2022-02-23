d06744af7781c1c90d0a7d478ec637b46c4513c3
IsaiasNG <isaiasnunogalindo@gmail.com>
2022-02-23 18:06:30 +0000

    Resolve "Fix navbar issues"

---
+++ frontend/src/pages/dashboardPage/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../../components/navbarComponent/Navbar';
+import styles from './dashboard.module.css';
+
+export default function Dashboard() {
+    return (
+        <>
+            {/* <div className={styles.dashboardContainer}> */}
+            <Navbar />
+            {/* </div> */}
+        </>
+    );
+}
