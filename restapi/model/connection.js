8538dbfbbf197ddb4a52a0c9bd13748aaca03c9e
Nick Raffaele <Raffaele.nickp@gmail.com>
2022-02-23 15:23:56 -0500

    Ssl off

--- restapi/model/connection.js
+++ restapi/model/connection.js
@@ -4,6 +4,9 @@ const connectionString = process.env.DATABASE_URL;
 
 const pool = new pg.Pool({
     connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 export default pool;
