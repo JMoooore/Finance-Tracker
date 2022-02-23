5ef4a3b3da4fd0b6150a4df6d77c3b84f2c16a39
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-02-23 11:05:35 -0600

    Get Full Data for each user route

---
+++ restapi/controllers/usersController 2.js
@@ -0,0 +1,30 @@
+import userModel from '../model/usersModel.js';
+
+const userController = {};
+
+userController.getAll = async (req, res) => {
+    const data = await userModel.getAll();
+    res.json(data);
+};
+
+userController.getOne = async (req, res) => {
+    const data = await userModel.getOne(req.params.id);
+    res.json(data);
+};
+
+userController.post = async (req, res) => {
+    const data = await userModel.post(req.body);
+    res.json(data);
+};
+
+userController.patch = async (req, res) => {
+    const data = await userModel.patch(req.params.id, req.body);
+    res.json(data);
+};
+
+userController.delete = async (req, res) => {
+    const data = await userModel.delete(req.params.id);
+    res.json(data);
+};
+
+export default userController;
