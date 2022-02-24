3cf75a48178c7b567e59e13af464a1804886b4d1
Nick Raffaele <Raffaele.nickp@gmail.com>
2022-02-24 17:14:44 -0500

    Fixed API url

--- frontend/src/config/apiUrl.js
+++ frontend/src/config/apiUrl.js
@@ -1,7 +1,7 @@
-const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+const apiBaseUrl = process.env.REACT_APP_BASE_API_URL;
 if (!apiBaseUrl) {
     console.error(
         'No API URL found. This application will not function correctly'
     );
 }
-export const API_BASE_URL = apiBaseUrl;
+export const BASE_API_URL = apiBaseUrl;
