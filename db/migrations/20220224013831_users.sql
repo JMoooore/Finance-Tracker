b67e7b1c151fa3882d7e388565144e874e0e87e5
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-02-24 17:11:17 +0000

    Sql migrations

--- db/migrations/20220224013831_users.sql
+++ db/migrations/20220224013831_users.sql
@@ -1,5 +1,13 @@
 -- migrate:up
+CREATE TABLE users(
+    id serial PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    password VARCHAR(255),
+    email VARCHAR(255)
+);
 
 
 -- migrate:down
+DROP TABLE users;
 
--- db/migrations/20220224013913_payees.sql
+++ db/migrations/20220224013913_payees.sql
@@ -1,5 +1,11 @@
 -- migrate:up
+CREATE TABLE payees(
+    id serial PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    name VARCHAR(100)
+);
 
 
 -- migrate:down
+DROP TABLE payees;
 
