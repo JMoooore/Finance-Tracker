efd15954c035dffa3f022041836d8d4e080e1438
Nick Raffaele <Raffaele.nickp@gmail.com>
2022-02-23 20:35:58 -0500

    Fixed typo

--- restapi/model/connection.js
+++ restapi/model/connection.js
@@ -5,7 +5,7 @@ const connectionString = process.env.DATABASE_URL;
 const pfConfig = { connectionString };
 
 if (environment === 'prod') {
-    pfconfig.ssl = { rejectUnauthorized: false };
+    pfConfig.ssl = { rejectUnauthorized: false };
 }
 
 const pool = new pg.Pool(pfConfig);
