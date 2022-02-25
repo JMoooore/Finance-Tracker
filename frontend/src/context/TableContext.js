bfc1ac24d01a24ef2ee9674b17e7a41388af7639
IsaiasNG <isaiasnunogalindo@gmail.com>
2022-02-25 16:17:49 -0600

    updates

--- frontend/src/context/TableContext.js
+++ frontend/src/context/TableContext.js
@@ -15,7 +15,7 @@ export const TableProvider = ({ children }) => {
 
     const postNewAccount = async (obj) => {
         const response = await axios.post(`/accounts/forUser${userData.id}`, obj);
-        return response.id;
+        return response.data.id;
     };
 
     const postNewPayee = async (obj) => {
@@ -25,7 +25,7 @@ export const TableProvider = ({ children }) => {
 
     const postNewCategory = async (obj) => {
         const response = await axios.post(`/categories/forUser${userData.id}`, obj);
-        return response[0].id;
+        return response.data[0].id;
     };
 
     const deleteTransaction = async (oldTransaction) => {
@@ -39,31 +39,24 @@ export const TableProvider = ({ children }) => {
         setUserData({ ...userData, transactions: newArr });
     };
 
-    const postNewTrans = async (userId, obj, newData) => {
-        const response = await axios.post('/transactions/forUser1', obj);
-        setUserData({ ...userData, transactions: [...transactions, newData] });
+    const postNewTrans = async (obj) => {
+        const response = await axios.post(`/transactions/forUser${userData.id}`, obj);
     };
 
     const addTransaction = async (userInput) => {
-        console.log(userInput)
         const data = {};
         data.user_id = userData.id;
         data.inflow = userInput.inflow;
         data.outflow = userInput.outflow;
+
         const payee = userData.payees.find(payee => payee.name == userInput.payee_name)
-        console.log(payee);
         if (payee) {
-            console.log("in here 1");
             data.payee_id = payee.id
         } else {
-            console.log("in here 2");
             const inputObj = {user_id: data.user_id, name:userInput.payee_name}
             data.payee_id = await postNewPayee(inputObj)
-            console.log("in here 3");
         }
        
-console.log(data);
-
         const category = userData.categories.find(category => category.name == userInput.category_name)
         if (category) {
             data.category_id = category.id
@@ -72,27 +65,18 @@ console.log(data);
             data.category_id = await postNewCategory(inputObj)
         }
 
-console.log(data);
-
 
         const account = userData.accounts.find(account => account.name == userInput.account_name)
+        console.log(account)
         if (account) {
             data.account_id = account.id
         } else {
             const inputObj = {user_id: data.user_id, name:userInput.account_name}
             data.account_id = await postNewAccount(inputObj)
         }
-        data.account_id = userData.accounts.find(
-            (account) => account.name == userInput.account_name
-        ).id;
-        // if(data.payee_id === undefined || data.payee_id === null){
-        //     let accountObj = {}
-        //     accountObj.user_id = data.user_id
-        //     accountObj.name = userInput.account_name
-        //     data.account_id = await postNewAccount(data.user_id, accountObj)
-        // }
-        console.log(data);
+
         const response = await axios.post(`/transactions/forUser${data.user_id}`, data);
+        const newData = {...data, ...userInput}
         const [{ id }] = response.data;
         userInput.transaction_id = id;
         userData.transactions.push(userInput);
